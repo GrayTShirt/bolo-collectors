e6e2a1924878219e56df26aa2e375ac380d7b537
James Hunt <james@niftylogic.com>
2015-03-18 18:45:05 -0400

    Finish file collector

--- src/files.c
+++ src/files.c
@@ -9,6 +9,13 @@
 #include <sys/stat.h>
 #include <fts.h>
 #include <fnmatch.h>
+#include <vigor.h>
+
+static char *PREFIX;
+static int32_t ts;
+
+#define MIN(a,b) ((a) > (b) ? (b) : (a))
+#define MAX(a,b) ((a) < (b) ? (b) : (a))
 
 #define OP_NOT  3
 #define OP_AND  2
@@ -62,6 +69,31 @@ static const char *PR_TYPE_NAMES[] = {
 #define MATCH_EQ  0
 #define MATCH_LT -1
 
+#define AGGREGATE_SUM 1
+#define AGGREGATE_MIN 2
+#define AGGREGATE_MAX 3
+#define AGGREGATE_AVG 4
+
+#define TRACK_SIZE    1
+#define TRACK_COUNT   2
+
+typedef struct {
+	char *path;
+	int   time_start;
+	int   xdev;
+	int   aggregate;
+	int   track;
+	int   debug;
+	int   dumptree;
+
+	uint64_t count;
+	struct {
+		uint64_t min;
+		uint64_t max;
+		uint64_t sum;
+	} size;
+} context_t;
+
 typedef struct {
 	pr_t type;
 	int match;
@@ -88,6 +120,7 @@ typedef struct {
 	int  i;
 	int  argc;
 	char **argv;
+	int  debug;
 
 	expr_t *rpn[PARSER_RPN_MAX];
 	int     len;
@@ -300,7 +333,8 @@ static expr_t* s_parse(parser_t *p)
 	}
 
 	while (p->top > 0) {
-		printf("despooling [%i] (%02x)\n", p->top, p->ops[p->top - 1]);
+		if (p->debug)
+			printf("despooling [%i] (%02x)\n", p->top, p->ops[p->top - 1]);
 		s_push_rpn(p, make_oper(p->ops[--p->top]));
 	}
 
@@ -399,24 +433,94 @@ static void s_expr_dump(expr_t *e, const char *prefix)
 	free(new);
 }
 
+static void s_track(context_t *c, FTSENT *e)
+{
+	c->count++;
+	if (c->count == 1) {
+		c->size.min = c->size.max = e->fts_statp->st_size;
+	} else {
+		c->size.min = MIN(c->size.min, e->fts_statp->st_size);
+		c->size.max = MAX(c->size.max, e->fts_statp->st_size);
+	}
+	c->size.sum += e->fts_statp->st_size;
+}
+
+static void s_usage(const char *bin)
+{
+	fprintf(stderr, "USAGE: %s /path/to/dir [check-options] -- [find-options]\n", bin);
+	exit(1);
+}
+
 int main(int argc, char **argv)
 {
-	if (argc < 2) {
-		fprintf(stderr, "USAGE: %s /path/to/dir [check-options] -- [find-options]\n", argv[0]);
-		return 1;
+	if (argc < 2)
+		s_usage(argv[0]);
+
+	context_t ctx = { 0 };
+	ctx.track     = TRACK_COUNT;
+	ctx.aggregate = AGGREGATE_SUM;
+
+	ctx.path = strdup(argv[1]);
+	if (ctx.path[0] == '-')
+		s_usage(argv[0]);
+
+	int i;
+	for (i = 2; i < argc - 1; i++) {
+		if (strcmp(argv[i], "--") == 0) {
+			i++;
+			break;
+		}
+		if (strcmp(argv[i], "-debug") == 0) {
+			ctx.debug = 1;
+			continue;
+		}
+		if (strcmp(argv[i], "-dumptree") == 0) {
+			ctx.dumptree = 1;
+			continue;
+		}
+		if (strcmp(argv[i], "-track") == 0) {
+			i++; if (!argv[i]) s_usage(argv[0]);
+
+			     if (strcasecmp(argv[i], "count") == 0) ctx.track = TRACK_COUNT;
+			else if (strcasecmp(argv[i], "size")  == 0) ctx.track = TRACK_SIZE;
+			else s_usage(argv[0]);
+			continue;
+		}
+		if (strcmp(argv[i], "-aggregate") == 0 || strcmp(argv[i], "-aggr") == 0) {
+			i++; if (!argv[i]) s_usage(argv[0]);
+
+			if (strcasecmp(argv[i], "sum") == 0)
+				ctx.aggregate = AGGREGATE_SUM;
+			else if (strcasecmp(argv[i], "min") == 0 || strcasecmp(argv[i], "minimum") == 0)
+				ctx.aggregate = AGGREGATE_MIN;
+			else if (strcasecmp(argv[i], "max") == 0 || strcasecmp(argv[i], "maximum") == 0)
+				ctx.aggregate = AGGREGATE_MAX;
+			else if (strcasecmp(argv[i], "avg") == 0 || strcasecmp(argv[i], "average") == 0)
+				ctx.aggregate = AGGREGATE_AVG;
+			else s_usage(argv[0]);
+		}
 	}
 
-	char *path = strdup(argv[1]);
-	if (path[0] == '-') {
-		fprintf(stderr, "USAGE: %s /path/to/dir [check-options] -- [find-options]\n", argv[0]);
-		return 1;
+	if (ctx.track == TRACK_COUNT && ctx.aggregate != AGGREGATE_SUM) {
+		fprintf(stderr, "WARNING: you specified -track count with a -aggr %s, which makes no sense.  "
+		                "falling back to -aggr sum\n",
+			  ctx.aggregate == AGGREGATE_MIN ? "min"
+			: ctx.aggregate == AGGREGATE_MAX ? "max"
+			: ctx.aggregate == AGGREGATE_AVG ? "avg" : "<unknown>");
 	}
 
-	parser_t p = { .i = 2, .argc = argc, .argv = argv };
+	if (!argv[i])
+		s_usage(argv[0]);
+
+	parser_t p = { .i = i, .argc = argc, .argv = argv, .debug = ctx.dumptree };
 	expr_t *root = s_parse(&p);
-	s_expr_dump(root, "");
+	if (ctx.dumptree) {
+		fprintf(stderr, "\neval parse tree:\n");
+		s_expr_dump(root, "");
+		fprintf(stderr, "\n");
+	}
 
-	char * const paths[2] = { path, NULL };
+	char * const paths[2] = { ctx.path, NULL };
 	FTS *f = fts_open(paths, FTS_PHYSICAL, NULL);
 	if (!f) {
 		perror("fts_open");
@@ -426,8 +530,43 @@ int main(int argc, char **argv)
 	FTSENT *e;
 	while ((e = fts_read(f)) != NULL) {
 		if (e->fts_info & FTS_DP) continue;
-		if (s_eval(root, e))
-			fprintf(stderr, "FOUND (%i) %s [%s]!\n", e->fts_level, e->fts_path, e->fts_name);
+		if (s_eval(root, e)) {
+			if (ctx.debug) {
+				fprintf(stderr, "found file `%s' [%lub]\n", e->fts_path, e->fts_statp->st_size);
+			}
+			s_track(&ctx, e);
+		}
+	}
+
+	if (ctx.debug) {
+		fprintf(stderr, "filesystem traversal complete\n");
+		fprintf(stderr, "final stats:\n");
+		fprintf(stderr, "  count:     %lu\n", ctx.count);
+		fprintf(stderr, "  min(size): %lu\n", ctx.size.min);
+		fprintf(stderr, "  max(size): %lu\n", ctx.size.max);
+		fprintf(stderr, "  sum(size): %lu\n", ctx.size.sum);
+		fprintf(stderr, "  avg(size): %f\n",  1.0 * ctx.size.sum / ctx.count);
+	}
+
+	PREFIX = fqdn();
+	ts = time_s();
+	if (ctx.track == TRACK_COUNT) {
+		printf("SAMPLE %i %s:files:NAME %lu\n", ts, PREFIX, ctx.count);
+
+	} else if (ctx.track == TRACK_SIZE) {
+		printf("SAMPLE %i %s:files:NAME ", ts, PREFIX);
+
+		switch (ctx.aggregate) {
+		case AGGREGATE_SUM: printf("%lu\n", ctx.size.sum); break;
+		case AGGREGATE_MIN: printf("%lu\n", ctx.size.min); break;
+		case AGGREGATE_MAX: printf("%lu\n", ctx.size.max); break;
+		case AGGREGATE_AVG: printf("%e\n",  1.0 * ctx.size.sum / ctx.count); break;
+		default: printf("0\n");
+		}
+
+	} else {
+		fprintf(stderr, "invalid -track option\n");
+		return 1;
 	}
 
 	return 0;
